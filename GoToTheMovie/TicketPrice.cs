89e5cec041c084957a0f5679377d29910cf6f9e7
soile@soile.se <soile@soile.se>
2020-08-28 14:22:35 +0200

    klassfilrer - extreherade

---
+++ GoToTheMovie/TicketPrice.cs
@@ -0,0 +1,25 @@
+﻿namespace GoToTheMovie
+{
+    public class TicketPrice
+    {
+        public int thePrice;
+
+
+        public TicketPrice(int thePrice)
+        {
+            this.thePrice = thePrice;
+        }
+
+        public int GetThePrice()
+        {
+            return thePrice;
+        }
+
+        public void SetThePrice(int thePrice)
+        {
+            this.thePrice = thePrice;
+        }
+
+    }
+}
+
